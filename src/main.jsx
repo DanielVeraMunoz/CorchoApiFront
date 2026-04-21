0e20222b9d91be7bfac038a4b17bf1a24cae542d
Daniel Vera <dani.vmg91@gmail.com>
2026-04-21 16:24:56 +0200

    Change url in .env because it was wrong

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import { AuthProvider } from './context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
 
